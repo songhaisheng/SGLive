2c51919bbe7cf9cf68beeab50ded1b58483f4049
BoBo <bobo@BoBodeMac-mini.local>
2017-01-11 18:31:06 +0800

    推流完善

---
+++ SGLive/SGPushViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SGPushViewController.swift
+//  SGLive
+//
+//  Created by BoBo on 17/1/10.
+//  Copyright © 2017年 SG. All rights reserved.
+//
+
+import UIKit
+
+class SGPushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
